d71fd2aca5411a4c7b90f01011b8a6e48e248753
Ariel Hern <hernandez.ariel1228@gmail.com>
2020-08-07 14:54:08 -0400

    added current user to apollo server context

--- server/index.js
+++ server/index.js
@@ -1,28 +1,41 @@
-const { ApolloServer } = require('apollo-server')
-const mongoose = require('mongoose')
+const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
-const { typeDefs } = require('./typedefs')
-const { resolvers } = require('./resolvers')
+const User = require('./model/user');
+const { typeDefs } = require('./typedefs');
+const { resolvers } = require('./resolvers');
 
-const config = require('./utils/config')
+const config = require('./utils/config');
 
 
-console.log('connecting to databse')
+console.log('connecting to databse');
 mongoose.connect(config.MONGODB_URI, {
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
     useCreateIndex: true
 }).then(() => {
-    console.log('succesfuly connected to MongoDB')
+    console.log('succesfuly connected to MongoDB');
 }).catch((error) => {
-    console.log('error connecting to mongoDB', error.message)
-})
+    console.log('error connecting to mongoDB', error.message);
+});
+
+
+const context = async ({ req }) => {
+    const authorization = req ? req.headers.authorization : null
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        const decodedToken = jwt.verify(authorization.substring(7), config.JWT_SECRET)
+        const currentUser = await User.findById(decodedToken.id)
+        return { currentUser }
+    }
+}
 
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    context
 })
 
 
