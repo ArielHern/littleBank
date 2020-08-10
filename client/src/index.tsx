9180479a58a18c8abd756fb765911c043aed3efb
Ariel Hern <hernandez.ariel1228@gmail.com>
2020-08-10 14:04:17 -0400

    add token to header

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,21 +3,44 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import {
-    ApolloProvider, ApolloClient, HttpLink, InMemoryCache
-} from '@apollo/client';
+    ApolloClient,
+    ApolloProvider,
+    HttpLink,
+    InMemoryCache
+} from '@apollo/client'
+import { setContext } from 'apollo-link-context'
 
 import 'semantic-ui-css/semantic.min.css'
 
-const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    link: new HttpLink({
-        uri: 'http://localhost:4000',
-    })
+
+const authLink = setContext((_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const token = localStorage.getItem('littleBank-user-token');
+    // return the headers to the context so httpLink can read them
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : null,
+        }
+    }
 });
 
+const httpLink = new HttpLink({
+    uri: 'http://localhost:4000'
+})
 
 
-//ApolloProvider makes the client accessible for all components of the application
+const client = new ApolloClient({
+    link: authLink.concat(httpLink as any) as any,// Chain it with the HttpLink
+    cache: new InMemoryCache()
+})
+
+
+
+
+
+
+// ApolloProvider makes the client accessible for all components of the application
 ReactDOM.render(
     <ApolloProvider client={client}>
         <App />
