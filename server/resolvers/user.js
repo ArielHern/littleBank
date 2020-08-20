729cad17957a5f8f48ff59a22b8a42fbe23915c8
Ariel Hern <hernandez.ariel1228@gmail.com>
2020-08-19 22:35:09 -0400

    return all accounts for current users

--- server/resolvers/user.js
+++ server/resolvers/user.js
@@ -1,11 +1,17 @@
 const User = require('../model/user');
 const Transaction = require('../model/transaction');
+const Account = require('../model/account');
 
 module.exports = {
     resolvers: {
         Query: {
             me: async (_, args, { currentUser }) => {
                 return currentUser;
+            }
+        },
+        User: {
+            accounts: async (_, args, { currentUser }) => {
+                return await Account.find({ owner: currentUser });
             }
         }
     }
