3f8a16657f23c65919187edc295279bbf1b4a6af
Ariel Hern <hernandez.ariel1228@gmail.com>
2020-08-22 09:06:24 -0400

    added User type, Home and account components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,32 @@
-import { useQuery, useSubscription } from '@apollo/client';
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, useRouteMatch } from 'react-router-dom';
+import { useQuery, useSubscription } from '@apollo/client';
 import { Container, Button } from 'semantic-ui-react';
 import LoadingPage from './components/LoadingPage';
 import LoginForm from './components/LoginForm';
 import TransactionForm from './components/transaction/TransactionForm';
 import Transactions from './components/transaction/Transactions';
-import { BALANCE, BALANCE_CHANGED } from './graphql/queries';
+import Home from './components/Home';
+import Account from './components/account/Account';
+import { ME, BALANCE_CHANGED } from './graphql/queries';
 
+import { User } from './graphql/types';
 
-interface Balance {
-    balance: number
+interface IMe {
+    me: User
 }
 
 function App() {
     const [token, setToken] = React.useState<string>('');
     const [showTransactions, setShowTransactions] = React.useState<boolean>(false);
     const [AccountBalance, setAccountBalance] = React.useState(0);
-    const { loading, data } = useQuery<Balance>(BALANCE, {
-        onCompleted: (data) => setAccountBalance(Math.round(data.balance)),
-        onError: (error) => console.log(error.graphQLErrors[0].message)
-    });
+    //const { loading, data } = useQuery<Balance>(BALANCE, {
+    //    onCompleted: (data) => setAccountBalance(Math.round(data.balance)),
+    //    onError: (error) => console.log(error.graphQLErrors[0].message)
+    //});
+
+    const { data, loading } = useQuery<IMe>(ME)
+
 
     React.useEffect(() => {
         const token = localStorage.getItem('littleBank-user-token')
@@ -52,21 +59,34 @@ function App() {
     }
 
     return (
-        <div style={{ marginTop: "10px" }}>
-            <Container textAlign="left"><h1>{`Balance: $${AccountBalance}`}</h1></Container>
-            <TransactionForm />
-            <Container>
-                <div style={{ marginTop: "20px" }}>
-                    <h3>Transactions history <Button
-                        size="mini"
-                        color={showTransactions ? "yellow" : "green"}
-                        onClick={showHideTransactions}>
-                        {showTransactions ? "Hide" : "View"}
-                    </Button></h3>
-                    {showTransactions ? <Transactions /> : null}
-                </div>
-            </Container>
+        <div>
+            <Router>
+                <Switch>
+                    <Route path='/accounts/:id'>
+                        <Account account={data?.me?.accounts!} />
+                    </Route>
+                    <Route path="/accounts">
+                        <Home />
+                    </Route>
 
+
+                    <div style={{ marginTop: "10px" }}>
+                        <Container textAlign="left"><h1>{`Balance: $${AccountBalance}`}</h1></Container>
+                        <TransactionForm />
+                        <Container>
+                            <div style={{ marginTop: "20px" }}>
+                                <h3>Transactions history <Button
+                                    size="mini"
+                                    color={showTransactions ? "yellow" : "green"}
+                                    onClick={showHideTransactions}>
+                                    {showTransactions ? "Hide" : "View"}
+                                </Button></h3>
+                                {showTransactions ? <Transactions /> : null}
+                            </div>
+                        </Container>
+                    </div>
+                </Switch>
+            </Router>
         </div>
 
     );
