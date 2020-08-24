5759afade8148e9914dc95a0f0bcdffe16f36964
Ariel Hern <hernandez.ariel1228@gmail.com>
2020-08-24 12:42:33 -0400

    added account name and transactions, if no transaction a message is display

--- client/src/components/account/Account.tsx
+++ client/src/components/account/Account.tsx
@@ -1,27 +1,46 @@
 import React from 'react'
-import { Container, Divider, Table } from 'semantic-ui-react'
+import { Container, Divider, Table, Button } from 'semantic-ui-react'
 import moment from 'moment';
 
-import { Account, Transaction } from '../../graphql/types';
+import { Transaction } from '../../graphql/types';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { TRANSACTIONS_HISTORY, ACCOUNT_INFO } from '../../graphql/queries';
 
-const AccountDisplay: React.FC<{ account: Account[] }> = ({ account }) => {
+
+
+const AccountDisplay: React.FC = () => {
+    //Get id from url
+    const { id } = useParams();
+
+    const { data, loading } = useQuery(TRANSACTIONS_HISTORY, {
+        variables: { id }
+    });
+
+    const { data: account } = useQuery(ACCOUNT_INFO, {
+        variables: { id }
+    });
 
 
     const formatDateFrom = (date: Date) => {
         return moment(date).format('MM-DD-YYYY');
     }
+    const buttonStyle = {
+        marginTop: "10px"
+    }
 
+    if (loading) return null;
 
     return (
         <div>
-            <Divider />
             <Container>
-                {account.map((acc: Account) =>
-                    <div key={acc.id}>
-                        <h2>{acc.name}</h2>
-                        <h3>Balance: ${acc.balance}</h3>
-
-                        <Container>
+                <hr />
+                <h2>{account?.accountInfo.name}</h2>
+                <hr />
+                <h5>Balance: ${account?.accountInfo.balance}</h5>
+                <Container>
+                    <div>
+                        {data ? (
                             <Table celled>
                                 <Table.Header>
                                     <Table.Row>
@@ -31,7 +50,7 @@ const AccountDisplay: React.FC<{ account: Account[] }> = ({ account }) => {
                                         <Table.HeaderCell>Description</Table.HeaderCell>
                                     </Table.Row>
                                 </Table.Header>
-                                {acc.transactions.map((transaction: Transaction, index: number) => {
+                                {data.transactions.edges.map((transaction: Transaction, index: number) => {
                                     return (
                                         <Table.Body key={index}>
                                             <Table.Row key={transaction.id}>
@@ -48,13 +67,14 @@ const AccountDisplay: React.FC<{ account: Account[] }> = ({ account }) => {
                                     )
                                 })}
                             </Table>
-                        </Container>
+
+                        )
+                            : <div> not Transactions</div>}
                     </div>
-
-                )}
-            </Container>
-
-        </div>
+                    <Button style={buttonStyle} size="small" color="teal">New Transaction</Button>
+                </Container>
+            </Container >
+        </div >
     )
 }
 
