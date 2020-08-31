e9e4db53b2427f2469afa6cb256c710a1d90cdd0
Ariel Hern <hernandez.ariel1228@gmail.com>
2020-08-31 16:07:40 -0400

    display last 5 transactions on home page

--- client/src/components/account/Accounts.tsx
+++ client/src/components/account/Accounts.tsx
@@ -1,17 +1,52 @@
 import React from 'react'
-import { Container } from 'semantic-ui-react';
+import { Container, Table } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 import { Account } from '../../graphql/types';
 
 const Accounts: React.FC<{ accounts: Account[] }> = ({ accounts }) => {
-
     return (
-        <Container>
-            <h2>Accounts</h2>
-            {accounts.map((acc) => <div key={acc.name}><b><Link to={`/accounts/${acc.id}`}>{acc.name}</Link></b> - ${acc.balance}</div>)}
-        </Container>
+        <div>
+            {accounts.map((acc) =>
+                <>
+                    <Container key={acc.name}>
+                        <b><Link to={`/accounts/${acc.id}`}>{acc.name}</Link></b> - ${acc.balance}
+                    </Container>
+
+                </>
+
+            )}
+            <div>
+                <Table basic="very" celled style={{ width: "400px", height: "100px", marginTop: "20px" }}>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell>Last Transactions</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+                    {accounts.map((acc) =>
+                        acc.transactions.slice(0, 4).map((transaction: any, index: number) =>
+                            (
+                                <React.Fragment key={index}>
+                                    <Table.Body >
+                                        <Table.Row key={transaction.id}>
+                                            <Table.Cell>{transaction.amount}</Table.Cell>
+                                            <Table.Cell>{transaction.type}</Table.Cell>
+                                            {transaction.memo
+                                                ? <Table.Cell>{transaction.memo}</Table.Cell>
+                                                : <Table.Cell></Table.Cell>
+                                            }
+                                        </Table.Row>
+                                    </Table.Body>
+                                </React.Fragment>
+                            )
+                        )
+                    )}
+                </Table>
+            </div>
+        </div>
+
     )
 }
 
 export default Accounts;
+
